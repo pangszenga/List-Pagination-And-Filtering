bcff42a7e3bedda3c64eb9c4f7848cb2c6ed73a8
Priscilla Pang <Priscilla.pang@keystonelaw.co.uk>
2019-02-03 16:55:54 +0000

    Finished

--- js/script.js
+++ js/script.js
@@ -3,22 +3,6 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-/******************************************
-ToDo:
-showPage()
-appendPageLinks()
-
-Page needs to be Paginated - display 10 items every time
-Page needs to append buttons depending on how many items there are
-Page needs to have search bar to search through List
-
-Create list of student-items - done
-
-Slice every 10 - done
-Give every 10 items an index
-Append to page
-
-******************************************/
 
 /******************************************
 
@@ -42,6 +26,9 @@ for (let i = 1; i <= numOfPages; i++)
 
   }; //loop for Number of Pages
 
+//Ul Component
+let ul = document.createElement("ul");
+ul.classList.add("student-list");
 
 //Pages content
 let studentItems = [];
@@ -53,15 +40,43 @@ for (let i = 0; i < studentList.length; i += 10)
 
   }; //for loop ends
 
+
+
 for (let i = 0; i < studentList.length; i ++)
   {
     studentNames.push(document.getElementsByTagName("h3")[i].innerHTML);
-
   }; //for loop ends
 
 
-console.log(studentItems);
-console.log(studentNames);
+//Default display 1st 10 studentItems
+for(let i = 0; i < studentItems[0].length; i ++)
+{
+  ul.innerHTML += `<li class="student-item cf"> ${studentItems[0][i].innerHTML} </li>`;
+  page.appendChild(ul);
+}; //for loop ends
+
+
+//Create items to append
+//Search Bar
+
+let searchBar = document.createElement("div");
+searchBar.classList.add("student-search");
+searchBar.innerHTML += `<input placeholder="Search for students..." type="text" id="input">
+                        <button>Search</button>`;
+
+//Buttons
+
+let pagination = document.createElement("div");
+pagination.classList.add("pagination");
+pagination.innerHTML += `<ul id= "buttons">`;
+
+for (let i = 0; i < numOfPagesArray.length; i++)
+{
+  pagination.getElementsByTagName("ul")[0].innerHTML += `<li ><a href="#${numOfPagesArray[i]}"> ${numOfPagesArray[i]} </a>`;
+
+}; //loop ends
+
+
 
 
 /******************************************
@@ -73,67 +88,48 @@ FUNCTIONS
 
 function appendPageLinks()
   {
-    //Create items to append
-    //Search Bar
-
-    let searchBar = document.createElement("div");
-    searchBar.classList.add("student-search");
-    searchBar.innerHTML += `<input placeholder="Search for students..." type="text" id="input" value="text">
-                            <button>Search</button>`;
-
-    //Buttons
-
-    let pagination = document.createElement("div");
-    pagination.classList.add("pagination");
-    pagination.innerHTML += "<ul>";
-
-    for (let i = 0; i < numOfPagesArray.length; i++)
-    {
-      pagination.getElementsByTagName("ul")[0].innerHTML += `<li><a href="#${numOfPagesArray[i]}"> ${numOfPagesArray[i]} </a>`;
-
-    }; //loop ends
-
     //Append items
     page.appendChild(pagination);
     pageHeader.appendChild(searchBar);
 
-  }//function ends
+  };//function ends
 
-
-//Event Listener for SearchBar
-
-//Event Listener for Buttons
 
 function showPage()
   {
-    //Create ul element
-    let ul = document.createElement("ul");
-    ul.classList.add("student-list");
-
     //Hide existing list
-    page.removeChild(originalUl);
-
-
-
-    //ONLY display 10 items at a time
-    for(let i = 0; i < studentItems[0].length; i ++){
-      //Default display 1st 10 studentItems
-      ul.innerHTML += `<li class="student-item cf"> ${studentItems[0][i].innerHTML} </li>`
-      page.appendChild(ul);
-      console.log(studentItems[0]);
-    }
+    originalUl.style.display = "none"
 
     //Append Buttons and SearchBar
     appendPageLinks();
 
-  }
+    //Functionality
+    //Pagination
+    let buttonUl = document.getElementById('buttons');
 
+    for (let i = 0; i < buttonUl.children.length; i++)
+    {
+        buttonUl.children[i].onclick = function()
+        {
+          console.log(i);
+          while (ul.firstChild)
+          {
+            ul.removeChild(ul.firstChild);
+          }
+          let selected = studentItems[i];
+
+          console.log(ul);
+          for (let j = 0; j < selected.length; j++)
+          {
+            console.log(selected[j]);
+            ul.innerHTML += `<li class="student-item cf"> ${selected[j].innerHTML} </li>`;
+            page.insertBefore(ul, pagination);
+          }; //for loop ends
+
+          }; //event listener ends
+
+    };//for loop ends
+
+  };//function ends
 
 showPage();
-
-// what should showPage do? contain search and page click links functionality
-//what should appendPageLinks do? append appropriate page when link is click and hide when not clicked
-
-
-//ul.innerHTML += `<li class="student-item cf"> ${studentItems[i].innerHTML} </li>`
-// page.appendChild(ul);
